73dcfea1b47edc9d659c95ba66b74bf3a131b26c
Irving Arreola Palacios <irving.a789@gmail.com>
2022-10-23 21:10:05 -0500

    test2 contact form

--- assets/js/contactForm.js
+++ assets/js/contactForm.js
@@ -1,117 +1,20 @@
-// const fields = {}
+const btn = document.getElementById('button');
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     fields.name = document.getElementById('name');
-//     fields.email = document.getElementById('email');
-//     fields.message = document.getElementById('message');
-
-
-//     function isNotEmpty(value) {
-//         if (value === null || typeof value === 'undefined') return false;
-//         return (value.length > 0);
-//     }
-
-//     function isEmail(email) {
-//         let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-//         return regex.test(String(email).toLowerCase());
-//     }
-
-//     function fieldValidation(field, validationFunction) {
-//         if (field === null) return false;
-
-//         let isFieldValid = validationFunction(field.value)
-//         if (!isFieldValid) {
-//             field.className = 'placeholderRed';
-//         } else {
-//             field.className = '';
-//         }
-
-//         return isFieldValid;
-//     }
-
-//     function isValid() {
-//         var valid = true;
-
-//         valid &= fieldValidation(fields.name, isNotEmpty);
-//         valid &= fieldValidation(fields.email, isNotEmpty);
-//         valid &= fieldValidation(fields.message, isNotEmpty);
-//         valid &= fieldValidation(fields.email, isEmail);
-
-//         return valid;
-//     }
-
-//     function isNotEmpty(value) {
-//     if(value === null || typeof value === 'undefined') return false;
-//     return (value.length > 0);
-// }
-
-// function isEmail(email) {
-//     let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-//     return regex.test(String(email).toLowerCase());
-// }
-
-// function fieldValidation(field, validationFunction) {
-//     if (field === null) return false;
-
-//     let isFieldValid = validationFunction(field.value)
-//     if (!isFieldValid) {
-//         field.className = 'placeholderRed';
-//     } else {
-//         field.className = '';
-//     }
-
-//     return isFieldValid;
-// }
-
-// function isValid() {
-//     var valid = true;
-
-//     valid &= fieldValidation(fields.name, isNotEmpty);
-//     valid &= fieldValidation(fields.email, isNotEmpty);
-//     valid &= fieldValidation(fields.message, isNotEmpty);
-//     valid &= fieldValidation(fields.email, isEmail);
-
-//     return valid;
-// }
-
-// class User {
-//     constructor(name, email, message) {
-//         this.name = name;
-//         this.email = email;
-//         this.message = message;
-//     }
-// }
-
-// function sendContact() {
-//     if(isValid()) {
-//         let usr = new User(name.value, email.value, message.value);
-//         alert("success")
-//     }else {
-//         alert("error")
-//     }
-
-// }
-
-// })\
-
-
-(function () {
-    // https://dashboard.emailjs.com/admin/account
-    emailjs.init('YOUR_PUBLIC_KEY');
-})();
-
-
-window.onload = function () {
-    document.getElementById('contact-form').addEventListener('submit', function (event) {
+document.getElementById('form')
+    .addEventListener('submit', function (event) {
         event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
-        // these IDs from the previous steps
-        emailjs.sendForm('contact_service', 'contact_form', this)
-            .then(function () {
-                console.log('SUCCESS!');
-            }, function (error) {
-                console.log('FAILED...', error);
+
+        btn.value = 'Sending...';
+
+        const serviceID = 'default_service';
+        const templateID = 'template_nhrq27f';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'Send Email';
+                alert('Sent!');
+            }, (err) => {
+                btn.value = 'Send Email';
+                alert(JSON.stringify(err));
             });
     });
-}
