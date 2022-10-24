0eab253d847f237bf4fbc4432ed903d8a749a635
Irving Arreola Palacios <irving.a789@gmail.com>
2022-10-23 21:41:55 -0500

    test6

--- assets/js/contactForm.js
+++ assets/js/contactForm.js
@@ -1,20 +1,20 @@
-const btn = document.getElementById('button');
+// const btn = document.getElementById('button');
 
-document.getElementById('form')
-    .addEventListener('submit', function (event) {
-        event.preventDefault();
+// document.getElementById('form')
+//     .addEventListener('submit', function (event) {
+//         event.preventDefault();
 
-        btn.value = 'Sending...';
+//         btn.value = 'Sending...';
 
-        const serviceID = 'default_service';
-        const templateID = 'template_nhrq27f';
+//         const serviceID = 'default_service';
+//         const templateID = 'template_nhrq27f';
 
-        emailjs.sendForm(serviceID, templateID, this)
-            .then(() => {
-                btn.value = 'Send Email';
-                alert('Sent!');
-            }, (err) => {
-                btn.value = 'Send Email';
-                alert(JSON.stringify(err));
-            });
-    });
+//         emailjs.sendForm(serviceID, templateID, this)
+//             .then(() => {
+//                 btn.value = 'Send Email';
+//                 alert('Sent!');
+//             }, (err) => {
+//                 btn.value = 'Send Email';
+//                 alert(JSON.stringify(err));
+//             });
+//     });
